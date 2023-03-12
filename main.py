2e397cee39a442ae264930c459a31ad1478d2696
wehaveagod <banerjeayush05@gmail.com>
2023-03-11 22:04:55 -0500

    Timer Countdown and End Screen

--- main.py
+++ main.py
@@ -22,27 +22,39 @@ def run_game():
 	#Clear screen
 	SCREEN.fill(BLACK)
 
-	#Running Boolean Variable
-	running = True
-
-	#Circle Characteristics
+	#Game Characteristics
 	CIRCLE_TIME = 1000
 	CIRCLE_RADIUS = 15
+	game_finished = False
 
-	#Variables for Circle and Time Tracking
-	circle_on_screen = False
+	#Circle variables
 	points = 0
+
+	circle_on_screen = False
 	circle_center = None
 	time_on_screen = None
 
+	#Clock variables
+	GAME_TIME = 10
+	timer_counter = GAME_TIME
+	timer_text = None
+
+	timer_event = pygame.USEREVENT + 1
+	pygame.time.set_timer(timer_event, 1000)
+
 	#Text Display for Points
-	FONT = pygame.font.Font('freesansbold.ttf', 16)
+	FONT = pygame.font.SysFont(None, 15)
 	points_text = FONT.render(str(points), True, WHITE, BLACK)
 	TEXT_RECT = points_text.get_rect()
 	TEXT_RECT.center = (WIDTH - 20, HEIGHT - 20)
 
 	#Game will run until "running" boolean is false
-	while running:
+	while not game_finished:
+		if timer_counter > 10:
+			timer_text = FONT.render(f"0: {timer_counter}", True, WHITE, BLACK)
+		else:
+			timer_text = FONT.render(f"0: 0{timer_counter}", True, WHITE, BLACK)
+
 		#Displaying the points text on the screen
 		SCREEN.blit(points_text, TEXT_RECT)
 
@@ -52,22 +64,20 @@ def run_game():
 			circle_center = (rand.randint(CIRCLE_RADIUS, WIDTH - CIRCLE_RADIUS), rand.randint(CIRCLE_RADIUS, HEIGHT - CIRCLE_RADIUS))
 			pygame.draw.circle(SCREEN, WHITE, circle_center, CIRCLE_RADIUS)
 			circle_on_screen = True
-			pygame.display.update()
 
 		#Remove Circle if it is on Screen for More than CIRCLE_TIME
 		elif(pygame.time.get_ticks() - time_on_screen >= CIRCLE_TIME):
 			pygame.draw.circle(SCREEN, BLACK, circle_center, CIRCLE_RADIUS)
 			circle_on_screen = False
-			pygame.display.update()
 
 		#Event Loop
 		for event in pygame.event.get():
 			#Quit game if Window is Closed
 			if event.type == pygame.QUIT:
-				running = False
+				pygame.quit()
 
 			#Detect if Mouse if Pressed
-			if event.type == pygame.MOUSEBUTTONUP:
+			elif event.type == pygame.MOUSEBUTTONUP:
 				#Get the Mouse Position
 				click_pos = pygame.mouse.get_pos()
 
@@ -81,8 +91,21 @@ def run_game():
 					#Removing the Circle After it is Pressed
 					pygame.draw.circle(SCREEN, BLACK, circle_center, CIRCLE_RADIUS)
 					circle_on_screen = False
-					pygame.display.update()
-	pygame.quit()
+
+			elif event.type == timer_event:
+				timer_counter -= 1
+				if timer_counter == 0:
+					game_finished = True
+				elif timer_counter > 10:
+					timer_text = FONT.render(f"0: {timer_counter}", True, WHITE, BLACK)
+				else:
+					timer_text = FONT.render(f"0: 0{timer_counter}", True, WHITE, BLACK)
+				
+		
+		timer_text_rect = timer_text.get_rect(center = (WIDTH - 20, 20))
+		SCREEN.blit(timer_text, timer_text_rect)
+
+		pygame.display.update()
 
 def start_screen():
 	START_BUTTON = button.Button(color = RED, x = 300, y = 200, width = 200, height = 100, text = "Start")
@@ -97,5 +120,24 @@ def start_screen():
 			if(event.type == pygame.QUIT):
 				pygame.quit()
 
+def end_screen() -> bool:
+	SCREEN.fill(BLACK)
+
+	RESET_BUTTON = button.Button(color = RED, x = 300, y = 200, width = 200, height = 100, text = "Reset")
+	RESET_BUTTON.draw(SCREEN)
+
+	pygame.display.update()
+
+	while True:
+		for event in pygame.event.get():
+			if(event.type == pygame.MOUSEBUTTONUP and RESET_BUTTON.is_over_mouse(pygame.mouse.get_pos())):
+				return False
+			if(event.type == pygame.QUIT):
+				pygame.quit()
+
+exit = False
+
 start_screen()
-run_game()
+while not exit:
+	run_game()
+	exit = end_screen()
